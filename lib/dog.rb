68a8e6efeedb553dbe4ffba7e5852a2929892508
Jacob David Diehl <jakediehl17@gmail.com>
2023-01-30 11:18:05 -0700

    commit tree a1b759ccd48c63d72cb59572a4340a9c5d38ee28

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+    attr_accessor :name, :breed
+def initialize (name)
+    @name = name
+end
+def initialize (breed)
+    @breed = breed
+end
 
 end
+
+
+fido = Dog.new(1)
+fido.name = "fido"
+fido.breed = "lab"
+puts fido.name
+puts fido.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
-
+attr_accessor :name, :job
+    def initialize(name)
+        @name = name
+    end
+    def initialize(job)
+        @job = job
+    end
 end
